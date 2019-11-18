377d11faafbcc824636d5773ab46dfa980c4803e
Goodmanc1998 <goodmanc1998@gmail.com>
2019-11-18 15:13:20 +0000

    Spelunky Style Room Gen
    Generates clean path from top of the level to the bottom of the level then fills in the rest of the level with random rooms

---
+++ AI-PCG_GridBased/Assets/Scripts/RoomType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomType : MonoBehaviour
+{
+    //Storing the rooms type
+    public int roomType;
+
+    //Function called to destroy the room
+    public void DestoyRoom()
+    {
+        Destroy(gameObject);
+    }
+}
